96b13f7a56cbd33e6b2bcd0cf2e25545861551b0
Guoxiaobo <313721439@qq.com>
2022-02-10 18:52:08 +0800

    no message

--- api/src/app/index.js
+++ api/src/app/index.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
 const KoaBody = require('koa-body')
 const router = require('../router')
+const staticFiles = require('koa-static')
+const path = require('path')
 const httpUtils = require('../utils/httpUtils')
 const errHandler = require('./errHandler')
 const { REMOTE_SETTINGS_PATH } = require('../config/config.default')
@@ -9,6 +11,8 @@ const app = new Koa()
 // app.use(KoaBody())
 app.use(KoaBody({multipart: true,formLimit:'1mb'}));
 app.use(router.routes()).use(router.allowedMethods())
+
+app.use(staticFiles(path.join(__dirname, '../../public')));
 
 // 统一监听错误事件
 app.on('error', errHandler)
