2c625854da96cfbca3bca9d2a539983405b2f0b3
Guoxiaobo <313721439@qq.com>
2022-02-11 20:15:34 +0800

    端口改变

--- cms/vite.config.js
+++ cms/vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:3130',
         changeOrigin: true,
         // rewrite: path => path.replace(/^\/api/, '')
       }
